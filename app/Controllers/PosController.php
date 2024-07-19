ae28ebc6471348e7dfd00f18e112855933fc28da
Pedro <pedroforjobs@outlook.com>
2024-07-19 16:16:15 -0300

    controller de PDV search Item ok

--- app/Controllers/PosController.php
+++ app/Controllers/PosController.php
@@ -1,11 +1,39 @@
 <?php
 
 namespace App\Controllers;
+use CodeIgniter\HTTP\Request;
+use Config\Database;
 
 class PosController extends BaseController
 {
     public function index()
     {
         return view('pos');
+    }
+
+    public function searchItem()
+    {
+        $query = $this->request->getVar('query');;
+        
+        $db = Database::connect();
+
+        $builderProducts = $db->table('products');
+
+        $builderServices = $db->table('services');
+
+        $builderProducts->like('name', $query);
+        $builderServices->like('name', $query);
+
+        $products = $builderProducts->get()->getResult();
+        $services = $builderServices->get()->getResult();
+
+        $combinedResults = array_merge($products, $services);
+
+        if (count($combinedResults) > 0) {
+            return $this->response->setJSON(['message' => 'success', 'data' => $combinedResults]);
+        } else {
+            return $this->response->setJSON(['message' => 'error', 'data' => 'Produto ou Serviço não encontrado!']); 
+        }
+
     }
 }
