181c97b89b4d6e2ca4366579f7a14d42585f8164
Pedro <pedroforjobs@outlook.com>
2024-07-19 11:15:42 -0300

    terminando modal

--- app/Views/pos.php
+++ app/Views/pos.php
@@ -21,6 +21,21 @@
             </ul>
         </nav>
 
+        <!-- Campo de pesquisa de itens -->
+
+        <input type="text" id="searchItem" placeholder="Insira o código de barras do item ou digite o nome do item">
+
+
+        <!-- Modal para lista de produtos -->
+        <div id="modal-products">
+            <h5 id="modal-products-title">Selecione o Produto</h5>
+            <div id="modal-products-body">
+                <ul id="modal-product-list">
+                    <!-- Lista de produtos será inserida aqui -->
+                </ul>
+            </div>
+        </div>
+
 
     </div>
     <script src="./js/pos.js"></script>
