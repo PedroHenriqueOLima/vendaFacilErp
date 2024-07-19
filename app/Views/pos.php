c787c34046b21f6a22de8507649eb0faba8b0ed6
Pedro <pedroforjobs@outlook.com>
2024-07-19 10:05:29 -0300

    criando estrutura para o PDV

---
+++ app/Views/pos.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/pos.css">
+    <title>Venda Fácil - PDV</title>
+</head>
+<body>
+    <h1>Venda Fácil</h1>
+    <script src="./js/pos.js"></script>
+</body>
+</html>
