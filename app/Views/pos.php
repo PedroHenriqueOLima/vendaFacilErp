dd4225849cdcd0fa653fc0405d12d5f2fc207c9a
Pedro <pedroforjobs@outlook.com>
2024-07-19 10:48:48 -0300

    navbar pronta

--- app/Views/pos.php
+++ app/Views/pos.php
@@ -7,11 +7,22 @@
     <link rel="stylesheet" href="./css/pos.css">
     <title>Venda Fácil - PDV</title>
 </head>
-<body>  
+<body>
     <div id="container">
-        
+        <nav id="navbar">
+            <div id="nav-brand">
+                <h1>PDV Venda Fácil</h1>
+                <small>Ph Tech Soluções</small>
+            </div>
+
+            <ul id="nav-links">
+            <li><a href="#" id="btn-panel">Painel Do Administrador</a></li>
+            <li><a href="./sale" id="btn-close-system">Sair do Sistema</a></li>
+            </ul>
+        </nav>
+
+
     </div>
-    <h1>Venda Fácil</h1>
     <script src="./js/pos.js"></script>
 </body>
 </html>
