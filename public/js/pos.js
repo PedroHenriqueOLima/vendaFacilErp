f003f7e61b278d64d0ae8d7e2d02d37a7d2b6fb1
Pedro <pedroforjobs@outlook.com>
2024-07-23 13:31:00 -0300

    organizando pos.js

--- public/js/pos.js
+++ public/js/pos.js
@@ -1,28 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const modalProducts = document.getElementById("modal-products");;
+  // Elementos do DOM
+  const modalProducts = document.getElementById("modal-products");
   const modalProductsList = document.getElementById("modal-products-list");
   const searchItem = document.getElementById("search-item");
   const btnCloseSystem = document.getElementById("btn-close-system");
-  const btnCloseModalProducts = document.getElementById(
-    "btn-close-modal-products"
-  );
-  let productsSaleList = document.getElementById("products-list-body");
+  const btnCloseModalProducts = document.getElementById("btn-close-modal-products");
+  const productsSaleList = document.getElementById("products-list-body");
 
-  // Variavel de contagem dos items
-
+  // Variáveis
   let totalItems = 0;
 
-  btnCloseSystem.addEventListener("click", () => {
-    closeSystem();
-  });
+  // Eventos
+  btnCloseSystem.addEventListener("click", closeSystem);
+  btnCloseModalProducts.addEventListener("click", closeModalProducts);
+  searchItem.addEventListener("input", handleSearchInput);
 
-  // Função de busca 
-  searchItem.addEventListener("input", (event) => {
-
+  // Funções
+  function handleSearchInput(event) {
     const query = event.target.value.trim();
-
     console.log(query.length);
-    // Verifica se é um código de barras (ajuste o comprimento conforme necessário)
+
     if (isNumeric(query) && (query.length === 12 || query.length === 13)) {
       if (isBarcode(query)) {
         searchByCode(query);
@@ -37,228 +34,150 @@ document.addEventListener("DOMContentLoaded", function () {
         } else {
           modalProducts.style.display = "none";
         }
-      })
+      });
     } else {
       modalProducts.style.display = "none";
     }
 
     event.preventDefault();
-  });
+  }
 
-  // Função de busca pelo código de barras
   function searchByCode(query) {
     fetch("/pdv/pesquisar-produto", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        query: query,
-      }),
+      body: JSON.stringify({ query }),
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json(); // Converter a resposta para JSON
-      })
-      .then((data) => {
-        console.log(data);
+      .then(handleResponse)
+      .then(data => {
         if (data.status === "ok") {
-          const product = data.data; 
-          addProductToSale(product);
+          addProductToSale(data.data);
           searchItem.value = "";
         } else {
           console.error("Unexpected response status:", data.status);
         }
       })
-      .catch((error) => {
-        console.error("There was a problem with the fetch operation:", error);
-      });
+      .catch(handleError);
   }
 
-  // Fechando o modal
-
-  btnCloseModalProducts.addEventListener("click", () => {
-    modalProducts.style.display = "none";
-    modalProductsList.innerHTML = "";
-  });
-
-  // Função de busca pelo nome
   function searchByName(query) {
     fetch("/pdv/pesquisar-produto", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        query: query,
-      }),
+      body: JSON.stringify({ query }),
     })
-      .then((response) => {
-        if (response.ok) {
-          return response.json(); // Parse the JSON from the response
-        } else {
-          throw new Error("Network response was not ok.");
-        }
-      })
-      .then((result) => {
-        console.log(result); // Log the full result object for debugging
-        if (result.status === "ok") {
-          const products = result.data; // Extract the products from the `data` key
-          addProductsToModal(products); // Pass the products to your function
+      .then(handleResponse)
+      .then(result => {
+        if (result.status === 'ok') {
+          addProductsToModal(result.data);
         } else {
           alert(result.message);
           searchItem.value = "";
         }
       })
-      .catch((error) => {
-        console.error(error);
-      });
+      .catch(handleError);
   }
 
-  // Expressão para verificar se a query é um código de barras ou não
+  function handleResponse(response) {
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
+    }
+    return response.json();
+  }
+
+  function handleError(error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
 
   function isBarcode(query) {
     return /^\d{13}$/.test(query);
   }
 
-  // Função para verificar se a entrada é um número
-
   function isNumeric(input) {
     return /^\d+$/.test(input);
   }
-
-  // Função para adicionar produtos a lista de produtos
 
   function addProductsToModal(products) {
     modalProducts.style.display = "block";
     modalProductsList.innerHTML = "";
 
-    // Create table and its elements
     const table = document.createElement("table");
     table.id = "product-table";
 
-    // Create table header
     const thead = document.createElement("thead");
     const headerRow = document.createElement("tr");
-
-    const nameHeader = document.createElement("th");
-    nameHeader.textContent = "Nome";
-    headerRow.appendChild(nameHeader);
-
-    const quantityHeader = document.createElement("th");
-    quantityHeader.textContent = "Estoque";
-    headerRow.appendChild(quantityHeader);
-
-    const priceHeader = document.createElement("th");
-    priceHeader.textContent = "Preço";
-    headerRow.appendChild(priceHeader);
-
-    const buttonHeader = document.createElement("th");
-    buttonHeader.textContent = "Adicionar a Venda";
-    headerRow.appendChild(buttonHeader);
-
+    headerRow.innerHTML = `
+      <th>Nome</th>
+      <th>Estoque</th>
+      <th>Preço</th>
+      <th>Adicionar a Venda</th>
+    `;
     thead.appendChild(headerRow);
     table.appendChild(thead);
 
-    // Create table body
     const tbody = document.createElement("tbody");
-
-    products.forEach((product) => {
+    products.forEach(product => {
       const row = document.createElement("tr");
-
-      const nameCell = document.createElement("td");
-      nameCell.textContent = product.name;
-      row.appendChild(nameCell);
-
-      const quantityCell = document.createElement("td");
-      quantityCell.textContent = product.quantity;
-      row.appendChild(quantityCell);
-
-      const priceCell = document.createElement("td");
-      priceCell.textContent = `R$ ${product.price}`;
-      row.appendChild(priceCell);
-
-      const buttonCell = document.createElement("td");
-      const button = document.createElement("button");
-      button.textContent = "Incluir na Venda";
-      button.addEventListener("click", () => {
+      row.innerHTML = `
+        <td>${product.name}</td>
+        <td>${product.quantity}</td>
+        <td>R$ ${product.price}</td>
+        <td><button class="btn">Incluir na Venda</button></td>
+      `;
+      row.querySelector("button").addEventListener("click", () => {
         addProductToSale(product);
         modalProducts.style.display = "none";
       });
-      buttonCell.appendChild(button);
-      row.appendChild(buttonCell);
-
       tbody.appendChild(row);
     });
-
     table.appendChild(tbody);
     modalProductsList.appendChild(table);
   }
 
-  // Função para adicionar produtos a lista de vendas
-
   function addProductToSale(product) {
-    // Definindo a quantidade como um item por padrão
-
-    let quantity = 1;
-
-    // Adicionando + 1 a quantidade de items da venda
-
-    // Registrando cada produto em uma linha
-
-    totalItems += 1;
     const row = document.createElement("tr");
+    const quantity = 1;
+    totalItems += 1;
 
-    let orderNumber = document.createElement("td");
-    orderNumber.textContent = totalItems;
-    row.appendChild(orderNumber);
-
-    const nameCell = document.createElement("td");
-    nameCell.textContent = product.name;
-    row.appendChild(nameCell);
-
-    const quantityCell = document.createElement("td");
-    quantityCell.textContent = quantity;
-
-    const priceCell = document.createElement("td");
-    priceCell.textContent = `R$ ${product.price}`;
-
-    const totalCell = document.createElement("td");
-    totalCell.textContent = `R$ ${(product.price * quantity).toFixed(2)}`;
-
-    const actionCell = document.createElement("td");
-
-    const removeButton = document.createElement("button");
-    removeButton.textContent = "Remover";
-    removeButton.classList.add("btn", "btn-remove-product");
-
-    removeButton.addEventListener("click", () => {
+    row.innerHTML = `
+      <td>${totalItems}</td>
+      <td>${product.name}</td>
+      <td>${quantity}</td>
+      <td>R$ ${product.price}</td>
+      <td>R$ ${(product.price * quantity).toFixed(2)}</td>
+      <td><button class="btn btn-remove-product">Remover</button></td>
+    `;
+    
+    row.querySelector(".btn-remove-product").addEventListener("click", () => {
       removeProductFromSale(row);
       totalItems -= 1;
-      orderNumber.textContent = orderNumber - 1;
+      updateOrderNumbers();
     });
-    actionCell.appendChild(removeButton);
-
-    row.appendChild(orderNumber);
-    row.appendChild(nameCell);
-    row.appendChild(quantityCell);
-    row.appendChild(priceCell);
-    row.appendChild(totalCell);
-    row.appendChild(actionCell);
 
     productsSaleList.appendChild(row);
-
     searchItem.value = "";
   }
 
-  // Função para remover produtos da lista de vendas
-
   function removeProductFromSale(row) {
-    const parent = row.parentNode;
-    parent.removeChild(row);
+    row.parentNode.removeChild(row);
   }
+
+  function updateOrderNumbers() {
+    const rows = productsSaleList.querySelectorAll("tr");
+    rows.forEach((row, index) => {
+      row.querySelector("td").textContent = index + 1;
+    });
+  }
+
+  function closeModalProducts() {
+    modalProducts.style.display = "none";
+    modalProductsList.innerHTML = "";
+  }
+
   function closeSystem() {
     window.location.href = "/login";
   }
